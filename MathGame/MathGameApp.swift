c09221c4d33f71d2e4404a6149555e99cd753f51
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2024-03-14 19:36:13 -0500

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Juan Camilo Marín Ochoa on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
